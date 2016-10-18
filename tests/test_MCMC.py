87be7a6cfc379b36e23d0a0f2b4d8cf511377fa8
Unni <ukurumba@u.rochester.edu>
2016-10-18 04:19:22 -0400

    Added unit tests and basic function framework.

--- tests/test_MCMC.py
+++ tests/test_MCMC.py
@@ -26,3 +26,27 @@ class TestMcmc(unittest.TestCase):
 
     def test_000_something(self):
         pass
+
+    def test_candidate_generator(self):
+    	def test_q(i):
+    		return 3*i + 12
+    	candidate = MCMC.q(test_q,2)
+    	self.assertEqual(candidate,18)
+
+    # def test_alpha_actually_minimizes(self):
+    # 	eq_distrib = [.2,.3,.2,.7]
+    	
+    # 	probability = MCMC
+
+
+
+
+tests = unittest.TestLoader().loadTestsFromTestCase(TestMcmc)
+unittest.TextTestRunner().run(tests)
+        
+
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
+
+
