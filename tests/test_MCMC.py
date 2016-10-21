79628599dfb01f0444630638e5997c22a2999375
Unni <ukurumba@u.rochester.edu>
2016-10-20 23:33:21 -0400

    trying to get to 100% coverage

--- tests/test_MCMC.py
+++ tests/test_MCMC.py
@@ -36,11 +36,24 @@ class TestMcmc(unittest.TestCase):
 
 
     def test_candidate_symmetry(self):
-    	Current_graph = [[0,1,1],[1,0,0],[1,0,0]]
     	grid = [[1.0,4.0],[3.0,7.0],[14.0,28.0]]
-    	candidate = MCMC.q(Current_graph,grid)
+    	init_graph = MCMC.initialization(grid)
+    	for i in range(0,10,1):
+    		candidate = MCMC.q(init_graph,grid)
     	val = np.allclose(candidate, np.transpose(candidate))
     	self.assertTrue(val)
+
+    def test_remove_link(self):
+    	grid = [[1.0,4.0],[3.0,7.0],[14.0,28.0]]
+    	graph = [[0,1,1],[1,0,1],[1,1,0]]
+    	candidate = MCMC.q(graph, grid)
+    	val = 0
+    	if candidate[0,0] == 0 or candidate[1,0] == 0 or candidate[2,0]==0:
+    		val = True 
+    	else:
+    		val = False 
+    	self.assertTrue(val)
+
 
     # def test_candidate_connected(self):
     # 	Current_graph = [[0,1,1],[1,0,0],[1,0,0]]
